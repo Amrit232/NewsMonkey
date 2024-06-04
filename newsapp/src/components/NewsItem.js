9ae54e591630e0bc88c9b6e7b2e98a12ab11839d
Amrit232 <amritanand232@gmail.com>
2024-06-04 15:13:55 +0530

    Previous and Next button has been added for traversing through pages

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} className="btn btn-primary">Get more info</a>
+              <a rel="noreference" href={newsUrl} className="btn btn-dark">Get more info</a>
             </div>
           </div>
       </div>
